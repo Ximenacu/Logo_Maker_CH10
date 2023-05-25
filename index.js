d5e39e6a516e31b3d0899a732ae088255c456cdf
Ximena Fernández del Castillo Cu <you@example.comximenacuu@gmail.com>
2023-05-24 23:49:23 -0600

    Testing the first test

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // npm init -y  (Generates package.json)
+ 
 // invoke app by: node index.js
 
 // Packages 
@@ -6,11 +7,14 @@ const inquirer = require('inquirer');
 // to install: npm i inquirer@8.2.4 (Generates node_modules, and package-lock.json. Adds inquirer to dependencies in package.json)
 const MaxLengthInputPrompt = require('inquirer-maxlength-input-prompt')
 // to install: npm i inquirer-maxlength-input-prompt
+
+// install JEST:
+// npm i jest
+
 const fs = require('fs');
 
-
+// begin: 
 inquirer.registerPrompt('maxlength-input', MaxLengthInputPrompt)
-
 let mydata={}
 inquirer
   .prompt([
