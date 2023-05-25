4828422c6fc82151a854564e12344817564ffd7b
Ximena Fernández del Castillo Cu <you@example.comximenacuu@gmail.com>
2023-05-24 21:19:59 -0600

    shape class done

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const inquirer = require('inquirer');
 // to install: npm i inquirer@8.2.4 (Generates node_modules, and package-lock.json. Adds inquirer to dependencies in package.json)
 const MaxLengthInputPrompt = require('inquirer-maxlength-input-prompt')
 // to install: npm i inquirer-maxlength-input-prompt
+const fs = require('fs');
+
 
 inquirer.registerPrompt('maxlength-input', MaxLengthInputPrompt)
 
@@ -27,16 +29,41 @@ inquirer
       type: 'list',
       message: 'Select a SHAPE',
       choices: ['Circle','Triangle','Square'],
-      name: 'shape',
+      name: 'name',
     },
     {
-        type: 'input',
-        message: 'Select a SHAPE COLOR. (Color keyword or hexadecimal number)',
-        name: 'shape color',
-      },
+      type: 'input',
+      message: 'Select a SHAPE COLOR. (Color keyword or hexadecimal number)',
+      name: 'fill',
+    },
+    {
+      type: 'input',
+      message: 'Name your svg file:',
+      name: 'fileName'
+    }
   ])
   .then( function savedata (response){
     mydata=response;
     console.log(mydata)
-  });
+    newFile(mydata);
+});
+
+const str = `<svg viewbox="0 0 100 100">
+
+<rect x="10" y="10" width="30" height="30"
+fill="red" />
+
+</svg>`
+
+// crear instancia dependiendo del tipo de forma, color, etc. 
+
+// 
+
+
+const newFile = (mydata) => {
+  fs.writeFile(`logo.svg`, str, (err) =>
+    err ? console.error(err) : console.log('Success!')
+  );
+} 
+
 
