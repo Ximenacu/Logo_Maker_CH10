460b1720eee69780a867d51e39f6470441c8741e
Ximena Fernández del Castillo Cu <you@example.comximenacuu@gmail.com>
2023-05-25 21:52:06 -0600

    Tests pass, logo is generated

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ const MaxLengthInputPrompt = require('inquirer-maxlength-input-prompt')
 
 const fs = require('fs');
 
+const {Circle, Triangle, Square} = require('./lib/shapes');
+
 // begin: 
 inquirer.registerPrompt('maxlength-input', MaxLengthInputPrompt)
 let mydata={}
@@ -22,27 +24,32 @@ inquirer
       type: 'maxlength-input',
       maxLength: 3,
       message: 'Input Initials (3)',
+      default: 'ABC',
       name: 'text',
     },
     {
       type: 'input',
       message: 'Select a TEXT COLOR. (Color keyword or hexadecimal number)',
+      default: 'red',
       name: 'color',
     },
     {
       type: 'list',
       message: 'Select a SHAPE',
       choices: ['Circle','Triangle','Square'],
+      default: 'Circle',
       name: 'name',
     },
     {
       type: 'input',
       message: 'Select a SHAPE COLOR. (Color keyword or hexadecimal number)',
+      default: 'white',
       name: 'fill',
     },
     {
       type: 'input',
       message: 'Name your svg file:',
+      default: 'logo',
       name: 'fileName'
     }
   ])
@@ -52,20 +59,26 @@ inquirer
     newFile(mydata);
 });
 
-const str = `<svg viewbox="0 0 100 100">
-
-<rect x="10" y="10" width="30" height="30"
-fill="red" />
-
-</svg>`
-
-// crear instancia dependiendo del tipo de forma, color, etc. 
-
-// 
-
-
 const newFile = (mydata) => {
-  fs.writeFile(`logo.svg`, str, (err) =>
+  let Logo;
+  switch (mydata.name){
+    case 'Circle':
+        console.log("A circle")
+        Logo = new Circle(mydata.text, mydata.color, mydata.fill);
+        break;
+      case 'Triangle':
+        console.log("A triangle")
+        Logo = new Triangle(mydata.text, mydata.color, mydata.fill);
+        break;
+      case 'Square':
+        console.log("A sqr")
+        Logo = new Square(mydata.text, mydata.color, mydata.fill);
+        break;
+  }
+  // const myLogo = new `${mydata.name}`(mydata.text, mydata.color, mydata.fill);
+  const str = Logo.render();
+  const fileNAme = mydata.fileName;
+  fs.writeFile(`./examples/${mydata.fileName}.svg`, str, (err) =>
     err ? console.error(err) : console.log('Success!')
   );
 } 
